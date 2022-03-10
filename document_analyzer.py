26eee222cd7f054e6ae3bc21aeaa13741069da57
seafu47 <60709886+seafu47@users.noreply.github.com>
2022-03-10 03:06:04 -0800

    Update document_analyzer.py

--- document_analyzer.py
+++ document_analyzer.py
@@ -10,12 +10,9 @@ def test():
 			x[word] = x[word] + 1		#increment word count
 		else:
 			x[word] = 1			#word count stay the same
-		
-		#for key in list(x.keys()): 
-		 # print ( key, ":" , x[key]) 
 
 	sortx = sorted( x, key = x.get, reverse= True) [:5] 	# sort the list in descending order
-	for i in range(len(sortx)-1):        			#loop through the 5 unique number
+	for i in range(len(sortx)):        			#loop through the 5 unique number
 		print(sortx[i], ":" , x[sortx[i]]) 		# print the word and repeated time
 
 		
