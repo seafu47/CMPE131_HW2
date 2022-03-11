2197b4bbde4506ecca72665d5998f01a50771a51
seafu47 <jiaheng.fang@sjsu.com>
2022-03-10 19:19:22 -0800

    update tracker.py

--- tracker.py
+++ tracker.py
@@ -1,15 +1,16 @@
-def func_counter(func)
+def func_counter(func):
   def wrapper():
         wrapper.counter += 1
         return wrapper
   wrapper.counter = 0
-  
+
   return wrapper
 
 @func_counter
 def foo():
     return 1
-  
-  foo()
-  foo()
-  print(foo.counter)
+
+foo()
+foo()
+
+print(foo.counter)
