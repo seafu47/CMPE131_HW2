37e5b58153db9fa98c9957702e9d4d8771ad82bd
seafu47 <60709886+seafu47@users.noreply.github.com>
2022-03-10 19:08:56 -0800

    Create tracker.py

---
+++ tracker.py
@@ -0,0 +1,15 @@
+def func_counter(func)
+  def wrapper():
+        wrapper.counter += 1
+        return wrapper
+  wrapper.counter = 0
+  
+  return wrapper
+
+@func_counter
+def foo():
+    return 1
+  
+  foo()
+  foo()
+  print(foo.counter)
